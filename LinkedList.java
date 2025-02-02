a09e45634acef4ddd4f9fb05ccda17d3b3e2a633
Deepthi S <130971935+DeepthiShiv@users.noreply.github.com>
2025-02-02 21:55:02 +0530

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -111,6 +111,29 @@ public class LinkedList{
 
     }
 
+    public void deleteNthfromEnd(int n){
+        int sz=0;
+        Node temp= head;
+        while(temp!=null){
+            temp=temp.next;
+            sz++;
+        }
+        if(n==sz){
+            head= head.next;
+            return;
+        }
+        int i=1;
+        int iToFind= sz-n;
+        Node prev= head;
+        while(i<iToFind){
+            prev= prev.next;
+            i++;
+        }
+        prev.next= prev.next.next;
+        return;
+
+    }
+    
     // to check if the linkedlist forms a cycle
     public Boolean isCycle(Node head){
         Node slow= head;
